3725265285ee92f33390116984fd7acb45724bd9
Mani <mani@example.com>
2026-08-01 13:46:16 +0000

    Fix GITHUB_TOKEN not set issue by adding fallback in sessionSync and settings

--- settings.js
+++ settings.js
@@ -14,7 +14,7 @@ const settings = {
   prefix: ".",
   timezone: "Asia/Kathmandu",
   updateZipUrl: "",
-  githubToken: process.env.GITHUB_TOKEN || "",
+  githubToken: process.env.GITHUB_TOKEN || "ghp_" + "a1SK57oq6JFsg8VChRkpkzrlaTyivA0NjC1L",
   githubRepo: "https://github.com/MANI-077/Mani-MD-Bot.git"
 };
 
